9af5a9b2e64ce134da20704d428edde030f0cc6f
rikuyaosawa <rikuyaosawa@gmail.com>
2025-01-26 18:13:57 +0900

    feat: complete header

---
+++ Scrimba/projects/TravelJournal/src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="/">
+                    <i className="fa-sharp fa-solid fa-earth-americas"></i>
+                </a>
+                <a href="/">
+                    <span>my travel journal.</span>
+                </a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
