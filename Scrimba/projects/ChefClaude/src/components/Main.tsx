5d6745450cce696f050e3541442887e0218f3b0f
rikuyaosawa <rikuyaosawa@gmail.com>
2025-02-02 15:28:18 +0900

    add the main content, form

---
+++ Scrimba/projects/ChefClaude/src/components/Main.tsx
@@ -0,0 +1,12 @@
+function Main() {
+    return (
+        <main>
+            <form>
+                <input type="text" placeholder="e.g. oregano" aria-label="Add ingredients" />
+                <button>Add ingredient</button>
+            </form>
+        </main>
+    );
+}
+
+export default Main;
