c2d73f93753431bf28c8c7cb43125556442c5737
rikuyaosawa <rikuyaosawa@gmail.com>
2025-02-02 15:08:48 +0900

    ChefClaude proj, create header

---
+++ Scrimba/projects/ChefClaude/src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="/">
+                    <img src="https://s3-alpha-sig.figma.com/img/a8a4/b5e0/a54b167a0c7a3a68ecd4597a20b4f005?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=av2rbWplEbb2g~rW1LpX026GhpqME1MKhfq~nyfUMQTT2ZXAM4MQqFmM8vv7PJDno7wfgYTdK1Xsuv6kfiDiufaZF7IF2BRq0iAGaiMLvp7UQgErmO3KjYsVa~4OLLRs~1MxvGcnx0i01l4vCQloPbKZL1bfvM9iJZDLiJ3flveTP0i0mCIz0WTsvZOFaWo9aoZ8kMoW~nsqojRUop5Y2qiQ19sX2yiJVK3RojKeQLoatNkc-P2v5d~6RgQXW1OLerxhQkzSpf4jw6SkvWrHG3qAadO1mP8XQYpItzZETC-n-hJa5KQV-prtZ4VUOAt5btV00KRB7nouIGUZpI--MQ__" />
+                    <h1>Chef Claude</h1>
+                </a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
