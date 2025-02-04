3f6f6338338677c9305a472919ebd7c0dd3470c7
rikuyaosawa <rikuyaosawa@gmail.com>
2025-02-04 19:17:58 +0900

    create main content with its styling

--- Scrimba/projects/ChefClaude/src/components/Main.tsx
+++ Scrimba/projects/ChefClaude/src/components/Main.tsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
+
 function Main() {
+    const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"]);
+
+    const ingredientItems = ingredients.map((item) => {
+        return <li key={item}>{item}</li>;
+    });
+
+    function log(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const ingredient = formData.get("ingredient");
+        if (ingredient !== null) {
+            ingredients.push(ingredient.toString());
+            setIngredients(ingredients);
+        }
+        console.log(ingredients);
+    }
+
     return (
         <main>
-            <form>
-                <input type="text" placeholder="e.g. oregano" aria-label="Add ingredients" />
-                <button>Add ingredient</button>
+            <form onSubmit={log}>
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredients"
+                    name="ingredient"
+                />
+                <button type="submit">Add ingredient</button>
             </form>
+            <div className="ingredientList">
+                <h1>Ingredients to use:</h1>
+                <ul>{ingredientItems}</ul>
+            </div>
+            <div className="callout">
+                <div className="content">
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button>Get a recipe</button>
+            </div>
         </main>
     );
 }
